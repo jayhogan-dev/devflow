360b68915d72d560ceebae34782cda26a8437f72
Jay Hogan <jayhogan.dev@gmail.com>
2023-10-04 15:22:41 -0700

    theme updated

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -1,15 +1,13 @@
-import Link from "next/link";
-import React from "react";
-import Image from "next/image";
-import { SignedIn, UserButton } from "@clerk/nextjs";
+import Link from 'next/link';
+import React from 'react';
+import Image from 'next/image';
+import { SignedIn, UserButton } from '@clerk/nextjs';
+import Theme from './Theme';
 
 const Navbar = () => {
   return (
     <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
-      <Link
-        href="/"
-        className="flex items-center gap-1"
-      >
+      <Link href="/" className="flex items-center gap-1">
         <Image
           src="/assets/images/site-logo.svg"
           alt="DevFlow logo"
@@ -23,16 +21,16 @@ const Navbar = () => {
       </Link>
       GlobalSearch
       <div className="flex-between gap-5">
-        Theme
+        <Theme />
         <SignedIn>
           <UserButton
             afterSignOutUrl="/"
             appearance={{
               elements: {
-                avatarBox: "h-10 w-10",
+                avatarBox: 'h-10 w-10',
               },
               variables: {
-                colorPrimary: "#ff7000",
+                colorPrimary: '#ff7000',
               },
             }}
           />
