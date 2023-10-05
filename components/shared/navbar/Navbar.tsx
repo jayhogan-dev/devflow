a7e8b8489b08a9510f115d5ef83dd566239dc193
Jay Hogan <jayhogan.dev@gmail.com>
2023-10-04 17:15:12 -0700

    navbar, theme, global search added

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -1,13 +1,18 @@
-import Link from 'next/link';
-import React from 'react';
-import Image from 'next/image';
-import { SignedIn, UserButton } from '@clerk/nextjs';
-import Theme from './Theme';
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import Theme from "./Theme";
+import MobileNav from "./MobileNav";
+import GlobalSearch from "../search/GlobalSearch";
 
 const Navbar = () => {
   return (
     <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
-      <Link href="/" className="flex items-center gap-1">
+      <Link
+        href="/"
+        className="flex items-center gap-1"
+      >
         <Image
           src="/assets/images/site-logo.svg"
           alt="DevFlow logo"
@@ -19,7 +24,7 @@ const Navbar = () => {
           Dev <span className="text-primary-500">Flow</span>
         </p>
       </Link>
-      GlobalSearch
+      <GlobalSearch />
       <div className="flex-between gap-5">
         <Theme />
         <SignedIn>
@@ -27,14 +32,16 @@ const Navbar = () => {
             afterSignOutUrl="/"
             appearance={{
               elements: {
-                avatarBox: 'h-10 w-10',
+                avatarBox: "h-10 w-10",
               },
               variables: {
-                colorPrimary: '#ff7000',
+                colorPrimary: "#ff7000",
               },
             }}
           />
         </SignedIn>
+        {/* <SignedOut>Login</SignedOut> */}
+        <MobileNav />
       </div>
     </nav>
   );
