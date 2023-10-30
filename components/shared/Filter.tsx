2e909e0a5e2ace0d8be7dbc27107b8b4cdf11e1c
Jay Hogan <jayhogan.dev@gmail.com>
2023-10-30 10:53:49 -0700

    filters added

--- components/shared/Filter.tsx
+++ components/shared/Filter.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import {
   Select,
   SelectContent,
+  SelectGroup,
   SelectItem,
   SelectTrigger,
   SelectValue,
@@ -19,13 +22,24 @@ const Filter = ({ filters, otherClasses, containerClasses }: Props) => {
   return (
     <div className={`relative ${containerClasses}`}>
       <Select>
-        <SelectTrigger className="w-[180px]">
-          <SelectValue placeholder="Theme" />
+        <SelectTrigger
+          className={`${otherClasses} body-regular light-border background-light800_dark300 text-dark500_light700 border px-5 py-2.5`}
+        >
+          <div className="line-clamp-1 flex-1 text-left">
+            <SelectValue placeholder="Select a Filter" />
+          </div>
         </SelectTrigger>
         <SelectContent>
-          <SelectItem value="light">Light</SelectItem>
-          <SelectItem value="dark">Dark</SelectItem>
-          <SelectItem value="system">System</SelectItem>
+          <SelectGroup>
+            {filters.map((item) => (
+              <SelectItem
+                key={item.value}
+                value={item.value}
+              >
+                {item.name}
+              </SelectItem>
+            ))}
+          </SelectGroup>
         </SelectContent>
       </Select>
     </div>
