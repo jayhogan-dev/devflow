98e813fb52bdc4e305f18d43fa599e3f46ae4163
Jay Hogan <jayhogan.dev@gmail.com>
2023-10-30 10:30:09 -0700

    filter component and select shadcn

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,5 +1,7 @@
+import Filter from "@/components/shared/Filter";
 import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import { Button } from "@/components/ui/button";
+import { HomePageFilters } from "@/constants/filters";
 import Link from "next/link";
 
 const Home = () => {
@@ -22,10 +24,15 @@ const Home = () => {
           route="/"
           iconPosition="left"
           imgSrc="/assets/icons/search.svg"
-          placeholder="Search for question"
+          placeholder="Search for questions"
           otherClasses="flex-1"
         />
-        Filters
+
+        <Filter
+          filters={HomePageFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+          containerClasses="hidden max-md:flex"
+        />
       </div>
     </>
   );
