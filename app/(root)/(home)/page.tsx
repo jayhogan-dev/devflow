b592109d5a3a0eca04ef8a6091939c1932991fd2
Jay Hogan <jayhogan.dev@gmail.com>
2023-10-30 15:45:55 -0700

    no result

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,48 @@
+import QuestionCard from "@/components/cards/QuestionCard";
 import HomeFilters from "@/components/home/HomeFilters";
 import Filter from "@/components/shared/Filter";
+import NoResult from "@/components/shared/NoResult";
 import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
 import Link from "next/link";
+
+const questions = [
+  {
+    _id: "1",
+    title: "Cascading Delete in SQLAlchemy",
+    tags: [
+      { _id: "1", name: "python" },
+      { _id: "2", name: "sql" },
+    ],
+    author: {
+      _id: "101",
+      name: "Jay Hogan",
+      picture: "https://example.com/path/to/jay_hogan_picture.jpg",
+    },
+    upvotes: 10,
+    views: 100,
+    answers: [],
+    createdAt: new Date("2021-09-01T12:00:00.000Z"),
+  },
+  {
+    _id: "2",
+    title: "How do I use express as a custom server in Nextjs?",
+    tags: [
+      { _id: "3", name: "nextjs" },
+      { _id: "4", name: "express" },
+    ],
+    author: {
+      _id: "102",
+      name: "Jay Hogan",
+      picture: "https://example.com/path/to/jay_hogan_picture2.jpg",
+    },
+    upvotes: 6,
+    views: 96,
+    answers: [],
+    createdAt: new Date("2023-10-01T12:00:00.000Z"),
+  },
+];
 
 const Home = () => {
   return (
@@ -35,7 +74,35 @@ const Home = () => {
           containerClasses="hidden max-md:flex"
         />
       </div>
+
       <HomeFilters />
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {questions.length > 0 ? (
+          questions.map((question) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
+        ) : (
+          <NoResult
+            title="There's no questions to show"
+            description="Be the first to break the silence! 🚀 Ask a Question and kickstart the
+          discussion. Your question could be the next big thing others learn from.
+          Get involved! 💡"
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />
+        )}
+      </div>
     </>
   );
 };
---
+++ components/cards/QuestionCard.tsx
@@ -0,0 +1,34 @@
+interface QuestionProps {
+  _id: string;
+  title: string;
+  tags: {
+    _id: string;
+    name: string;
+  }[];
+  author: {
+    _id: string;
+    name: string;
+    picture: string;
+  };
+  upvotes: number;
+  views: number;
+  answers: Array<object>;
+  createdAt: Date;
+}
+
+const QuestionCard = ({
+  _id,
+  title,
+  tags,
+  author,
+  upvotes,
+  views,
+  answers,
+  createdAt,
+}: QuestionProps) => {
+  return (
+    <div className="card-wrapper rounded-[10px] p-9 sm:px-11">QuestionCard</div>
+  );
+};
+
+export default QuestionCard;
